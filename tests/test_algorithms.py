89a2d6324e9e195e3872e3548b9cdf19219cabfd
Eryk Srebrny <esrebrny@onet.eu>
2025-09-15 12:40:14 +0200

    feat(tests): Create basic test template for testing algorithms using unittest

---
+++ tests/test_algorithms.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from umwl_geology.processing_provider.explode_table import ExplodeTableCode
+
+class TestAlgorithms(TestCase):
+
+    def test_exploding_table(self):
+        #      result = ExplodeTableCode(...).execute(...)
+        #      self.assertEqual(...)
+        pass
