5f7bf4efcf205ae208aceb89c91c98448ab8a488
Eryk Srebrny <esrebrny@onet.eu>
2025-09-16 11:22:04 +0200

    feat(plugin): enhance plugin api to add actions, retrieving processing provider and plugin main ui (action toolbar) as well as use observer pattern to unload plugin actions

--- umwl_geology/UMWLGeologyPlugin.py
+++ umwl_geology/UMWLGeologyPlugin.py
@@ -4,26 +4,79 @@ from qgis.pyqt.Widgets import QAction
 from qgis.pyqt.QtGui import QIcon
 from umwl_geology.processing_provider.provider import Provider
 
+
 class UMWLGeologyPlugin:
+
     def __init__(self, iface):
         self.iface = iface
         self.provider = None
+        self.cleanup_callbacks = []
+        self.plugin_menu_name = self.tr(u'&UMWL Geology')
+
+    def tr(self, message):
+        return QCoreApplication.translate('UMWL Geology', message)
+
+    def _addPluginAction(self,
+        icon_path,
+        text,
+        callback = None,
+        add_to_menu = True,
+        add_to_toolbar = True,
+        status_tip = None,
+        whats_this = None,
+        parent = None,
+        checkable = False,
+        enabled = True
+    ):
+
+        icon = QIcon(icon_path)
+        action = QAction(icon, text, parent)
+        action.triggered.connect(callback)
+        action.setEnabled(enabled)
+        action.setCheckable(checkable)
+
+        if status_tip is not None:
+            action.setStatusTip(status_tip)
+
+        if whats_this is not None:
+            action.setWhatsThis(whats_this)
+
+        if add_to_toolbar:
+            self.iface.addToolBarIcon(action)
+            self.cleanup_callbacks.append(lambda: self.iface.removeToolBarIcon(action))
+
+        if add_to_menu:
+            self.iface.addPluginToMenu(self.plugin_menu_name, action)
+            self.cleanup_callbacks.append(lambda: self.iface.removePluginMenu(self.plugin_menu_name, action))
+
+        return action
 
     def initProcessing(self):
         self.provider = Provider()
-        QgsApplication.processingRegistry().addProvider(self.provider)
+        qgs_processing_registry = QgsApplication.processingRegistry()
+        qgs_processing_registry.addProvider(self.provider)
+        self.cleanup_callbacks.append(lambda: qgs_processing_registry.removeProvider(self.provider))
 
     def initGui(self):
         self.initProcessing()
-        icon = QIcon(ASSETS_DIR / "coat_of_arms.png")
-        self.action = QAction(icon, 'Dummy Action', self.iface.mainWindow())
-        self.iface.addToolBarIcon(self.action)
-        self.action.triggered.connect(self.run)
+        self._addPluginAction(
+            ASSETS_DIR / "coat_of_arms.png"
+            'Dummy Action',
+            parent = self.iface.mainWindow()
+            callback = self._initPluginGUI
+        )
+
+    def _initPluginGUI(self):
+        # TODO
 
     def unload(self):
-        QgsApplication.processingRegistry().removeProvider(self.provider)
-        self.iface.removeToolBarIcon(self.action)
-        del self.action
+        for callback in self.cleanup_callbacks: callback()
+        self.cleanup_callbacks.clear()
 
-    def run(self):
-        self.iface.messageBar().pushMessage('Hello World from Plugin')
+    def getPluginActionToolbar(self):
+        # TODO
+
+    def getProcessingProvider(self):
+        if self.provider is None:
+            raise ValueError('Plugin processing was not initialized!')
+        return self.provider
