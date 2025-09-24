8a55c2b9ea1d43d6f4170b4f35eefe7c9a4a1216
Eryk Srebrny <esrebrny@onet.eu>
2025-09-24 14:55:51 +0200

    test: use pytest with pytest-qgis plugin to test qgis plugin and run simple coverage test

---
+++ tests/conftest.py
@@ -0,0 +1,5 @@
+import pytest
+
+@pytest.fixture
+def hello_world():
+    return 'Hello World !'
--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,9 +1,2 @@
-from unittest import TestCase
-from umwl_geology.processing_provider.explode_table import ExplodeTableCode
-
-class TestAlgorithms(TestCase):
-
-    def test_exploding_table(self):
-        #      result = ExplodeTableCode(...).execute(...)
-        #      self.assertEqual(...)
-        pass
+def test_hello(hello_world):
+    assert 'hello' in hello_world.lower()
