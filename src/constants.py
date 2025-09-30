da08bdd8c60e1b1d8611ca63872622b57ddbef98
Eryk Srebrny <esrebrny@onet.eu>
2025-09-30 11:27:13 +0200

    feat: add constants to plugin to be accessible globally

---
+++ src/constants.py
@@ -0,0 +1,9 @@
+from enum import IntEnum, auto
+
+
+class PolishAdministrativeLevel(IntEnum):
+    Voivodeship = auto()
+    County = auto()
+    Commune = auto()
+    Region = auto()
+    Parcel = auto()
